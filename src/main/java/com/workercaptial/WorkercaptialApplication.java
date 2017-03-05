dcbbb600b10fa71e0a7a3728a15510d37c0d4d3d
David Hope <email.djhope@gmail.com>
2017-03-04 19:39:52 -0500

    @Dave - initial check in

---
+++ src/main/java/com/workercaptial/WorkercaptialApplication.java
@@ -0,0 +1,12 @@
+package com.workercaptial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkercaptialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkercaptialApplication.class, args);
+	}
+}
